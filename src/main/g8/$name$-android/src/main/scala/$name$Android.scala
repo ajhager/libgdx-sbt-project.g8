1e473d6da68438aa8f19cfefe6f53959869f2b5e
Joseph Hager <aajhager@indiana.edu>
2011-04-09 14:14:45 -0400

    First stab at android project

---
+++ src/main/g8/$name$-android/src/main/scala/$name$Android.scala
@@ -0,0 +1,23 @@
+package $package$
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class $name$Android extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(new TextView(this) {
+      setText("hello, world")
+    })
+  }
+}
+
+//import com.badlogic.gdx.backends.android.AndroidApplication
+
+// class BaseAndroid extends AndroidApplication {
+//   override def onCreate(savedInstanceState: Bundle) {
+//     super.onCreate(savedInstanceState)
+//     initialize(new BaseGame(), true)
+//   }
+// }
