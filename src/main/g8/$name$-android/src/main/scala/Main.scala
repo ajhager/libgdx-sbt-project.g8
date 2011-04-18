02edbe0d26eff6b72ecfa19217c912b3d30833f0
Joseph Hager <aajhager@indiana.edu>
2011-04-18 08:25:20 -0400

    Attempting symbolic link

--- src/main/g8/$name$-android/src/main/scala/Main.scala
+++ src/main/g8/$name$-android/src/main/scala/Main.scala
@@ -1,29 +1,7 @@
 package $package$
 
 import android.os.Bundle
-
 import com.badlogic.gdx.backends.android.AndroidApplication
-import com.badlogic.gdx.ApplicationListener
-
-class MyGame extends ApplicationListener {
-  override def create() {
-  }
-
-  override def dispose() {
-  }
-
-  override def pause() {
-  }
-
-  override def render() {
-  }
-
-  override def resize(width: Int, height: Int) {
-  }
-
-  override def resume() {
-  }
-}
 
 class Main extends AndroidApplication {
   override def onCreate(savedInstanceState: Bundle) {
