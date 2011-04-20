3bbc1fb2c06b353d0132022f767edd56a777e6b4
Joseph Hager <aajhager@indiana.edu>
2011-04-19 23:35:00 -0400

    Hacked backend support using reflection

--- project/build/project.scala
+++ project/build/project.scala
@@ -2,27 +2,4 @@ import sbt._
 
 class TemplateProject(info: ProjectInfo) extends DefaultProject(info) with giter8.Template {
   override def disableCrossPaths = true
-
-  def capitalize(s: String) = {
-    s(0).toUpperCase + s.substring(1, s.length).toLowerCase
-  }
-  val props = myReadProps(defaultProperties)
-  props.setProperty("desktop_backend_cap",
-                    capitalize(props.getProperty("desktop_backend")))
-
-  override lazy val writeTemplates = applyTemplates(
-    templateSources,
-    templateOutput,
-    props
-  ) describedAs "Apply default parameters to input templates and write to " + 
-    templateOutput
-
-  def myReadProps(f: Path) = {
-    val p = new java.util.Properties
-    FileUtilities.readStream(f.asFile, log) { stm =>
-      p.load(stm)
-      None
-    }
-    p
-  }
 }
--- src/main/g8/desktop/src/main/scala/Main.scala
+++ src/main/g8/desktop/src/main/scala/Main.scala
@@ -1,9 +1,28 @@
 package $package$
 
-import com.badlogic.gdx.backends.$desktop_backend$._
+import com.badlogic.gdx.ApplicationListener
 
 object Main {
+  def run(appName: String, game: ApplicationListener, name: String,
+          width: Int, height: Int, useGL2: Boolean) {
+      val app = Class.forName(appName)
+      val con = app.getConstructor(classOf[ApplicationListener],
+                                   classOf[String],
+                                   classOf[Int],
+                                   classOf[Int],
+                                   classOf[Boolean])
+      con.newInstance(game, name, width.asInstanceOf[AnyRef],
+                      height.asInstanceOf[AnyRef],
+                      useGL2.asInstanceOf[AnyRef])
+  }
+
   def main(args: Array[String]): Unit = {
-    new $desktop_backend_cap$Application(new MyGame(), "$name$", 480, 320, false);
+    var appName = "com.badlogic.gdx.backends"
+    if ("$desktop_backend$" == "lwjgl")
+      appName += ".lwjgl.LwjglApplication"
+    else
+      appName += ".jogl.JoglApplication"
+
+    run(appName, new MyGame(), "Game4", 480, 320, false)
   }
 }
