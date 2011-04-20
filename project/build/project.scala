8359948d00f383dfb6bd46883080427b8784cd18
Joseph Hager <aajhager@indiana.edu>
2011-04-19 20:06:39 -0400

    Another try at backend support

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,6 +1,30 @@
 import sbt._
 
-class TemplateProject(info: ProjectInfo)
-    extends DefaultProject(info) with giter8.Template {
+class TemplateProject(info: ProjectInfo) extends DefaultProject(info) with giter8.Template {
   override def disableCrossPaths = true
+
+  def capitalize(s: String) = {
+    s(0).toUpperCase + s.substring(1, s.length).toLowerCase
+  }
+
+  val p = myReadProps(defaultProperties)
+  val c = capitialize(p.getProperty("desktop_backend"))
+
+  p.setProperty("desktop_backend_cap", c)
+
+  override lazy val writeTemplates = applyTemplates(
+    templateSources,
+    templateOutput,
+    p
+  ) describedAs "Apply default parameters to input templates and write to " + 
+    templateOutput
+
+  def myReadProps(f: Path) = {
+    val p = new java.util.Properties
+    FileUtilities.readStream(f.asFile, log) { stm =>
+      p.load(stm)
+      None
+    }
+    p
+  }
 }
--- src/main/g8/desktop/src/main/scala/Main.scala
+++ src/main/g8/desktop/src/main/scala/Main.scala
@@ -4,6 +4,6 @@ import com.badlogic.gdx.backends.$desktop_backend$._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    new $desktop_backend$Application(new MyGame(), "$name$", 480, 320, false);
+    new $desktop_backend_cap$Application(new MyGame(), "$name$", 480, 320, false);
   }
 }
