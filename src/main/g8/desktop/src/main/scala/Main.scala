d0a6247a13a80980f76215d2eec0e7b244b41275
Joseph Hager <ajhager@gmail.com>
2013-01-28 17:53:05 -0500

    Changed Desktop app to use App

--- src/main/g8/desktop/src/main/scala/Main.scala
+++ src/main/g8/desktop/src/main/scala/Main.scala
@@ -2,13 +2,11 @@ package $package$
 
 import com.badlogic.gdx.backends.lwjgl._
 
-object Main {
-  def main(args: Array[String]): Unit = {
+object Main extends App {
     val cfg = new LwjglApplicationConfiguration()
     cfg.title = "$name$"
     cfg.height = 480
     cfg.width = 320
     cfg.useGL20 = true
     new LwjglApplication(new MyGame(), cfg)
-  }
 }
