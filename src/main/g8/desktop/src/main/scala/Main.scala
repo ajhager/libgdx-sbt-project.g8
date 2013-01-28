ec55edb26fc86c3952ca6aed2fdbdedf80c62cfb
Joseph Hager <ajhager@gmail.com>
2013-01-28 17:42:43 -0500

    Minor fixes

--- src/main/g8/desktop/src/main/scala/Main.scala
+++ src/main/g8/desktop/src/main/scala/Main.scala
@@ -1,14 +1,14 @@
 package $package$
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val cfg = new LwjglApplicationConfiguration();
-    cfg.title = "$name$";
-    cfg.height = 480;
-    cfg.width = 320;
-    cfg.useGL20 = true;
-    new LwjglApplication(new MyLibgdxGame(), cfg);
+    val cfg = new LwjglApplicationConfiguration()
+    cfg.title = "$name$"
+    cfg.height = 480
+    cfg.width = 320
+    cfg.useGL20 = true
+    new LwjglApplication(new MyLibgdxGame(), cfg)
   }
 }
