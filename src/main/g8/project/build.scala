18012636ea3b7aa29b9b2441224556a78796e9c3
Joseph Hager <ajhager@gmail.com>
2013-03-05 22:57:11 -0500

    Fixed usage of glob filter

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -60,7 +60,7 @@ object Settings {
 
     s.log.info("Extracting desktop libs")
     val iosDest = file("ios/libs")
-    val iosFilter = new GlobFilter("ios/*")
+    val iosFilter = GlobFilter("ios/*")
     IO.unzip(zipFile, iosDest, iosFilter)
 
     s.log.info("Extracting android libs")
