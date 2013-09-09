9af107f3a249aacb0ea73ba4025d226c125e7944
Andrzej Giniewicz <Andrzej.Giniewicz@pwr.wroc.pl>
2013-09-09 14:31:25 +0200

    Simple changes to default settings
    Few simple changes to default settings to demonstrate useful options and
    follow recommended settings.
    
      - installLocation set to preferExternal
      - screenOrientation set to portrait
      - configChanges set to capture changes to keyboard and screen
      - hide status bar on Android devices
      - use OpenGL ES 1.x instead of 2.x
    
    For reference see http://code.google.com/p/libgdx/wiki/ApplicationConfiguration

--- src/main/g8/desktop/src/main/scala/Main.scala
+++ src/main/g8/desktop/src/main/scala/Main.scala
@@ -5,9 +5,9 @@ import com.badlogic.gdx.backends.lwjgl._
 object Main extends App {
     val cfg = new LwjglApplicationConfiguration()
     cfg.title = "$name$"
-    cfg.height = 480
     cfg.width = 320
-    cfg.useGL20 = true
+    cfg.height = 480
+    cfg.useGL20 = false
     cfg.forceExit = false
     new LwjglApplication(new $main_class$(), cfg)
 }
